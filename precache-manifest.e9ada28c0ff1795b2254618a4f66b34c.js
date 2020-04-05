50b2d03176fe6924a6b8adb8412e6d6e8f8c219a
bugagashinka <voloshyn.andrew@gmail.com>
2020-04-05 18:07:35 +0300

    Updates

---
+++ precache-manifest.e9ada28c0ff1795b2254618a4f66b34c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4190b99f353b67f290af",
+    "url": "/reactwarriors-movies-app/static/js/main.df37c951.chunk.js"
+  },
+  {
+    "revision": "cb9b1dee9f53158cd59e",
+    "url": "/reactwarriors-movies-app/static/js/runtime~main.4e301e02.js"
+  },
+  {
+    "revision": "0581ec53d3e98b901615",
+    "url": "/reactwarriors-movies-app/static/css/2.3d51ee82.chunk.css"
+  },
+  {
+    "revision": "0581ec53d3e98b901615",
+    "url": "/reactwarriors-movies-app/static/js/2.7d69ae37.chunk.js"
+  },
+  {
+    "revision": "f848a269d363f816b7e02f9ece63249d",
+    "url": "/reactwarriors-movies-app/index.html"
+  }
+];
